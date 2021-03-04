9420bfab414c2450dfcee29eb220f2ff5bd492ca
Asriel Yu <luffy84217@gmail.com>
2021-03-04 13:34:16 +0800

    build(core): add build system

--- index.ts
+++
@@ -1,7 +0,0 @@
-import Server from "./src/Server";
-import serverConfig from "./src/config/server";
-
-// Create a WebSocket server completely detached from the HTTP server.
-const wss = new Server(serverConfig);
-
-wss.listen();
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+import Server from "./Server";
+import serverConfig from "./config/server";
+
+const wss = new Server(serverConfig);
+
+wss.listen();
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
